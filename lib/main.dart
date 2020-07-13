3c1a516ffda3026a772e7237678efdde7315bc2c
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-07-12 20:54:48 -0500

    setting async functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/src/pages/home.dart';
+import 'package:flutter_components/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,46 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/providers/menu_provider.dart';
 
-class HomePageTemp extends StatelessWidget {
-  final opciones = ["Galería", "Mis Efectos", "Comparte"];
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Componentes"),
-      ),
-      body: ListView(
-        children: _crearItemsCorta(),
-      ),
+      appBar: AppBar(title: Text("Menu")),
+      body: _lista(),
     );
   }
 
-  List<Widget> _crearItems() {
-    List<Widget> lista = new List<Widget>();
-    for (String opt in opciones) {
-      final tempWidget = ListTile(
-        title: Text(opt),
-      );
-
-      lista..add(tempWidget)..add(Divider());
-    }
-
-    return lista;
+  Widget _lista() {
+    menuProvider.cargarData().then((resp) {});
+    return ListView(children: _crearMenu());
   }
 
-  List<Widget> _crearItemsCorta() {
-    return opciones.map((item) {
-      return Column(
-        children: <Widget>[
-          ListTile(
-            title: Text(item),
-            subtitle: Text("En desarrollo"),
-            leading: Icon(Icons.add_photo_alternate),
-            trailing: Icon(Icons.arrow_right),
-          ),
-          Divider()
-        ],
-      );
-    }).toList();
+  List<Widget> _crearMenu() {
+    return [
+      ListTile(title: Text("Hola")),
+      ListTile(title: Text("Hola")),
+      ListTile(title: Text("Hola")),
+    ];
   }
 }
